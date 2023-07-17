ce22d77c65d9f1f66d74ca0201bfd83991665b20
Karen Payne <paynekaren@comcast.net>
2023-07-16 17:59:22 -0700

    clean-up

--- createKaren.bat
+++ createKaren.bat
@@ -6,6 +6,6 @@ cd RazorSolution1
 :: create solution
 dotnet new sln -n RazorSolution1
 dotnet new karenrazorproj  -n SimpleWebApp -o SimpleWebApp
-::dotnet sln RazorSolution1.sln add SimpleWebApp\SimpleWebApp.csproj --solution-folder RazorPages
+dotnet sln RazorSolution1.sln add SimpleWebApp\SimpleWebApp.csproj --solution-folder RazorPages
 
 pause
